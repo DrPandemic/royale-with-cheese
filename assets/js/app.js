36109bfaa5ab9aac33df36f795230922fe085357
DrPandemic <bipbip500@gmail.com>
2018-11-23 11:47:35 -0500

    Prefill topbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,9 @@ import "phoenix_html"
 
 import Plotly from 'plotly.js-dist';
 import {boxplot7D, layout} from "./boxplot";
+import {preselectForm} from './topBar';
+
+preselectForm();
 
 const entries = JSON.parse(document.getElementById('boxplotChart').dataset['entries']);
 const data = boxplot7D(entries);
---
+++ assets/js/topBar.js
@@ -0,0 +1,21 @@
+import qs from "qs"
+
+export function preselectForm() {
+  selectDropdown(document.getElementById("region"), getUrlParam("region"));
+  selectDropdown(document.getElementById("realm"), getUrlParam("realm"));
+  document.getElementById("item_name").value = getUrlParam("item_name");
+}
+
+function getUrlParam(key) {
+  const url = window.location.search.substr(1);
+
+  return qs.parse(url)[key];
+}
+
+function selectDropdown(menu, value) {
+  for (const i in menu.options) {
+    if (menu.options[i].value === value) {
+      menu.options[i].selected = true;
+    }
+  }
+}

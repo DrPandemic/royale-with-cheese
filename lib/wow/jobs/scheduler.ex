b19ce7b0acf19176eb3e873bc613b4819684b9d1
DrPandemic <bipbip500@gmail.com>
2019-01-01 21:23:46 -0500

    Only download the needed icons

--- lib/wow/jobs/scheduler.ex
+++ lib/wow/jobs/scheduler.ex
@@ -24,12 +24,18 @@ defmodule Wow.Jobs.Scheduler do
   @spec schedule_item_icons(String.t) :: :ok | :err
   def schedule_item_icons(size) do
     if size == "36" || size == "56" do
-      result = Wow.Item.find_distinct_icons
-      result |> Enum.each(fn(name) -> Toniq.enqueue(Wow.Jobs.IconCrawler, name: name, size: size) end)
+      new_icons = Wow.Item.find_distinct_icons
+      new_icons -- downloaded_icons(size) |> Enum.each(fn(name) -> Toniq.enqueue(Wow.Jobs.IconCrawler, name: name, size: size) end)
 
       :ok
     else
       :err
     end
+  end
+
+  @spec downloaded_icons(String.t) :: [String.t]
+  defp downloaded_icons(size) do
+    Path.wildcard("./assets/static/images/blizzard/icons/#{size}/*.jpg")
+    |> Enum.map(fn name -> Regex.run(~r/.*\/(.*).jpg$/, name) |> List.last end)
   end
 end

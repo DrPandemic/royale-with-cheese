17493715e73683455c44c542e99fcf668cdcce6c
DrPandemic <bipbip500@gmail.com>
2019-01-01 19:12:18 -0500

    Adds median curve

--- assets/js/boxplot.js
+++ assets/js/boxplot.js
@@ -1,19 +1,43 @@
-import moment from 'moment-mini';
+import moment from "moment-mini";
 const defaultFormat = {
-  type: 'box',
+  type: "box",
   jitter: 0.3,
   pointpos: -1.5,
 };
 const defaultMarker = {
-  color: 'rgb(8,81,156)',
+  color: "rgb(8,81,156)",
+};
+const lineFormat = {
+  line: {
+    color: "rgb(253, 42, 42)",
+    width: 1.5
+  },
+  mode: "lines",
+  name: "median",
 };
 
 export function boxplot7D(entries, format, unit) {
   if (entries.every(e => e.length === 0)) {
     return [];
   }
-  let data = [];
-  let dates = [];
+
+  const [data, dates] = extractPrice(entries);
+
+  if (dates.length === 0) {
+    return [];
+  }
+
+  const fullDates = fillDates(dates, unit);
+  const sortedData = data.map(removeOutliers);
+  const boxplots = sortedData.map((val, i) => createSingleBoxplot(val, i, fullDates, format));
+  const line = createLine(sortedData, fullDates, format);
+
+  return [...boxplots, line];
+}
+
+function extractPrice(entries) {
+  const data = [];
+  const dates = [];
   for (const i in entries) {
     if (!data[i]) {
       data[i] = [];
@@ -27,43 +51,7 @@ export function boxplot7D(entries, format, unit) {
       data[i].push(entry.buyout / entry.quantity / 10000);
     }
   }
-
-  if (dates.length === 0) {
-    return [];
-  }
-
-  fillDates(dates, unit);
-
-  return data.map(removeOutliers)
-    .map((val, i) => {
-    if (val.length === 0) {
-      return {
-        y: [0],
-        name: dates[i].format(format),
-        boxpoints: showSingleValues() ? 'all' : false,
-        line: {
-          width: 0.2
-        },
-        marker: {
-          ...defaultMarker,
-          color: 'rgb(58,131,206)',
-        },
-        ...defaultFormat,
-      };
-    }
-    return {
-      y: val,
-      name: dates[i].format(format),
-      boxpoints: showSingleValues() ? 'all' : false,
-      line: {
-        width: 0.5
-      },
-      marker: {
-        ...defaultMarker
-      },
-      ...defaultFormat
-    };
-  });
+  return [data, dates];
 }
 
 function fillDates(dates, unit) {
@@ -79,13 +67,15 @@ function fillDates(dates, unit) {
       }
     }
   }
+  return dates;
 }
 
 function removeOutliers(data) {
+  data.sort((a, b) => a - b);
+
   if (data.length === 0 || !outlierSuppressionEnabled()) {
     return data;
   }
-  data.sort((a, b) => a - b);
 
   const median = percentile(data, 50);
   const q1 = percentile(data, 25);
@@ -93,6 +83,44 @@ function removeOutliers(data) {
   const iqr = (q3 - q1) * 1.5;
 
   return data.filter(e => e >= q1 - iqr && e <= q3 + iqr);
+}
+
+function createSingleBoxplot(val, i, dates, format) {
+  if (val.length === 0) {
+    return {
+      y: [0],
+      name: dates[i].format(format),
+      boxpoints: showSingleValues() ? "all" : false,
+      line: {
+        width: 0.2
+      },
+      marker: {
+        ...defaultMarker,
+        color: "rgb(58,131,206)",
+      },
+      ...defaultFormat,
+    };
+  }
+  return {
+    y: val,
+    name: dates[i].format(format),
+    boxpoints: showSingleValues() ? "all" : false,
+    line: {
+      width: 0.5
+    },
+    marker: {
+      ...defaultMarker
+    },
+    ...defaultFormat
+  };
+}
+
+function createLine(data, dates, format) {
+  return {
+    x: dates.map(d => d.format(format)),
+    y: data.map(v => v[Math.floor(v.length / 2)]),
+    ...lineFormat,
+  };
 }
 
 function percentile(list, n) {
@@ -108,16 +136,16 @@ function percentile(list, n) {
 }
 
 export const layout = {
-  title: 'Median price',
+  title: "Median price",
   yaxis: {
-    title: 'Price in gold per unit',
+    title: "Price in gold per unit",
   },
 }
 
 function outlierSuppressionEnabled() {
-  return document.getElementById('outlier-suppression').checked;
+  return document.getElementById("outlier-suppression").checked;
 }
 
 function showSingleValues() {
-  return document.getElementById('show-singles').checked;
+  return document.getElementById("show-singles").checked;
 }
